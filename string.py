9cd0361289c1cb8022b20e93cec819549e222f2a
NB320756 <ErnestM@nedbank.co.za>
2020-06-28 14:45:48 +0200

    added string file to learn strings

---
+++ string.py
@@ -0,0 +1,15 @@
+'''
+Basic Strings
+'''
+
+
+name = "Hello Ernest"
+#upper case
+print(name.upper())
+#lower case
+print(name.lower())
+value = "ernest"
+#make the first letter capital
+print(value.capitalize())
+
+print(",".join(["ernest", "lucy", "mamba"]))
