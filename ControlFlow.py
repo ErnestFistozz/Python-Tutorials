c085a1f88a954fe9857874319cb3144a2c7b92b5
NB320756 <ErnestM@nedbank.co.za>
2020-06-28 15:16:49 +0200

    learnig control flow

---
+++ ControlFlow.py
@@ -0,0 +1,9 @@
+#if, elif and else statements
+
+age = 13;
+if age < 16:
+    print("You are not eligible to vote")
+elif age >=16 and age <= 18:
+    print("You are not eligible to vote\nbut can register for the up coming elections")
+else:
+    print("You are eligible to vote")
