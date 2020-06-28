672ca30aff6b74115932fc1c3e90252554d4fb9c
NB320756 <ErnestM@nedbank.co.za>
2020-06-28 15:21:28 +0200

    learning, 'if', 'for' and 'while' statements

--- ControlFlow.py
+++ ControlFlow.py
@@ -6,4 +6,15 @@ if age < 16:
 elif age >=16 and age <= 18:
     print("You are not eligible to vote\nbut can register for the up coming elections")
 else:
-    print("You are eligible to vote")
+    print("You are eligible to vote" )
+
+# for loop statement
+
+for value in range(10):
+    print('Hello {}'.format(value))
+
+#while loop statement
+value = 0
+while value  != 5:
+    print("Hello Ernest, i am greeing for the {}th time".format(value))
+    value += 1
