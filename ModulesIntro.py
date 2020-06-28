3173a36b351167623ab0f27053989335c60597db
NB320756 <ErnestM@nedbank.co.za>
2020-06-28 16:07:54 +0200

    added basic version of guessing game with python

--- ModulesIntro.py
+++ ModulesIntro.py
@@ -3,3 +3,17 @@ import random
 
 for value in range(5):
     print(random.randint(1,10))
+
+#Guessing game using random function
+
+correct = random.randint(1,100)
+while True:
+    guess_number = int(input('Enter your guess: '))
+    if guess_number < correct:
+        print("You guessed lower!")
+    elif guess_number > correct:
+        print("You guessed higher!")
+    else:
+        print("You won! Bye")
+        break
+    continue
