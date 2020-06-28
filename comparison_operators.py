c57f327566e081798b3282c06e12801392f827ce
NB320756 <ErnestM@nedbank.co.za>
2020-06-28 14:51:19 +0200

    added operators file

---
+++ comparison_operators.py
@@ -0,0 +1,23 @@
+#Equal to
+
+value = 8
+value2  = 8
+# Equal to uses  == (double equals to)
+print(value == value2)
+
+#Not Equal to
+# Not equal uses !=
+
+print(value2 != value)
+
+#Greater than >
+print(value > value2)
+
+#Lesser than <
+print(value < value2)
+
+#Lesser or Equal to <=
+print(value2 <= value)
+
+#Grater than or equal to >=
+print(value2 >= value)
